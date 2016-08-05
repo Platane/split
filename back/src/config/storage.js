ad7739783bb3d931017ce1b71ef75d871da95648
Platane <me@platane.me>
2016-08-05 15:33:55 +0200

    :hammer: fix datastore option

--- back/src/config/storage.js
+++ back/src/config/storage.js
@@ -1,6 +1,8 @@
 
 module.exports = {
-    private_key     : process.env.SPLIT_GCLOUD_PRIVATE_KEY.replace( /\\n/g , '\n'),
-    client_email    : process.env.SPLIT_GCLOUD_MAIL,
+    credentials : {
+        private_key     : process.env.SPLIT_GCLOUD_PRIVATE_KEY.replace( /\\n/g , '\n'),
+        client_email    : process.env.SPLIT_GCLOUD_MAIL,
+    },
     projectId       : process.env.SPLIT_GCLOUD_PROJECTID,
 }
